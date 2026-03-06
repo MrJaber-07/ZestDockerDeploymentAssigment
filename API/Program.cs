eb1ed28dd1af4436fb512f6ae21f0b4355a814cb
MrJaber-07 <sjjaber5@gmail.com>
2026-03-06 12:35:52 +0530

    Correct MiddileWare

--- API/Program.cs
+++ API/Program.cs
@@ -4,84 +4,46 @@ using Infrastructure.Extensions;
 using Infrastructure.Security.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Serilog;
 
-try
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
-    Log.Information("Starting web host");
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
-    var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructureJwtAuthentication(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddEntityServices();
+builder.Services.AddControllers();
 
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
-
-    builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-    {
-        options.Password.RequireDigit = true;
-        options.Password.RequiredLength = 8;
-    })
-        .AddEntityFrameworkStores<ApplicationDbContext>()
-        .AddDefaultTokenProviders();
-
-    builder.Services.AddInfrastructureJwtAuthentication(builder.Configuration);
-    builder.Services.AddHttpContextAccessor();
-
-    builder.Services.AddEntityServices();
-    builder.Services.AddControllers();
-
-    var app = builder.Build();
-
-    using (var scope = app.Services.CreateScope())
-    {
-        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
-        string[] roles = new[] { "Admin", "User" };
-        foreach (var role in roles)
-        {
-            if (!await roleManager.RoleExistsAsync(role))
-                await roleManager.CreateAsync(new IdentityRole(role));
-        }
-
-        var adminEmail = builder.Configuration["Admin:Email"] ?? "admin@local";
-        var adminPwd = builder.Configuration["Admin:Password"] ?? "Admin123!";
-        var admin = await userManager.FindByEmailAsync(adminEmail);
-        if (admin == null)
-        {
-            admin = new ApplicationUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true, FirstName = "Sys", LastName = "Admin" };
-            var res = await userManager.CreateAsync(admin, adminPwd);
-            if (res.Succeeded)
-            {
-                await userManager.AddToRoleAsync(admin, "Admin");
-            }
-        }
-    }
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-
-    app.UseMiddleware<API.Middlewares.ExceptionMiddleware>();
-    app.UseHttpsRedirection();
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.MapControllers();
-  
-
-
-    app.Run();
-}
-catch (Exception ex)
+builder.Services.AddCors(options =>
 {
-    Log.Fatal(ex, "Host terminated unexpectedly");
-    throw;
-}
-finally
+    options.AddPolicy("MyReactPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // Your React URL
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    Log.CloseAndFlush();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<API.Middlewares.ExceptionMiddleware>();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
